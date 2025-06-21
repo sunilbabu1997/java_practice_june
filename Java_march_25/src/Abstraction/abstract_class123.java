20f0980b98cdd7b39a16fc8cc3f97abf02790be8
sunil <sunilnallapati120@gmail.com>
2025-06-21 22:10:10 +0530

    second commit

---
+++ Java_march_25/src/Abstraction/abstract_class123.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public class abstract_class123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the abstarct class");
+
+	}
+
+}
